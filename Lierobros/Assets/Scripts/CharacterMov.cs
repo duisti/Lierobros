0f6975d64dd8eee95fe56bfa979fb6e7648c8ced
VP Sassi <ovisa019@edu.xamk.fi>
2021-10-02 19:01:03 +0300

    duckman

--- Lierobros/Assets/Scripts/CharacterMov.cs
+++ Lierobros/Assets/Scripts/CharacterMov.cs
@@ -10,15 +10,35 @@ public class CharacterMov : MonoBehaviour
     public bool isGrounded = true;
     public float spriteRotation = 1;
     public float moveDir = 0;
+    public GameObject sprite;
     
     void Awake() {
         rigidBody = GetComponent<Rigidbody2D>();
+        sprite = GameObject.Find("Sprite");
+    }
+
+    void FixedUpdate() {
+        Movement();
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded) {
+            Jump();
+        }
     }
 
     void Movement() {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * speed;
         moveDir = movement.x;
+        SpriteFlipper(moveDir);
+    }
+
+    void SpriteFlipper(float moveDir) {
+        if (moveDir > 0) {
+            sprite.transform.localScale = new Vector3(1, sprite.transform.localScale.y, sprite.transform.localScale.z);
+        } 
+        if (moveDir < 0) {
+            sprite.transform.localScale = new Vector3(-1, sprite.transform.localScale.y, sprite.transform.localScale.z);
+        }
     }
 
     void Jump() {
