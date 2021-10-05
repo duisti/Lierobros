528ca3c0b8ff4df3f9abcfc626c928f6fd6694bc
DESKTOP-4ARKIPQ\JoonaLaakso <joona.julius.laakso@gmail.com>
2021-10-05 19:43:08 +0300

    Unnecessary old code graveyard and unused variables removed.

--- Lierobros/Assets/Scripts/Camera/CameraFollower.cs
+++ Lierobros/Assets/Scripts/Camera/CameraFollower.cs
@@ -7,8 +7,6 @@ public class CameraFollower : MonoBehaviour
 	[SerializeField]
 	private Transform target;
 	public float cameraSpeed = 15f; //base camera speed
-	public float maxDistance = 15f; //camera will never fall behind this distance
-	float divider = 1.5f; //this is used for the camera catchup
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +25,6 @@ public class CameraFollower : MonoBehaviour
 		}
 		Vector2 finalPosition = target.position;
 		Vector2 currentPosition = transform.position;
-		//check if we are way too far. If too far, we will move much faster towards our goal
-		/*
-		if (Vector2.Distance(finalPosition, currentPosition) > maxDistance) {
-			currentPosition = Vector2.MoveTowards(currentPosition, finalPosition, Vector2.Distance(finalPosition, currentPosition) - maxDistance);
-		}
-		*/
 		Vector2 lerpPosition = Vector2.Lerp(currentPosition, finalPosition, cameraSpeed * Time.deltaTime);
 		transform.position = lerpPosition;
 	}
